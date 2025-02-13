cb64efd1f2a5198c911e9671a44f2a6c0fbd56e8
omegafrog <jiwoo22110103@gmail.com>
2025-02-13 12:28:59 +0900

    feat: add post list

--- frontend/src/app/post/list/page.tsx
+++ frontend/src/app/post/list/page.tsx
@@ -6,7 +6,7 @@ export default async function PostList() {
   }
 
   const rsData = await response.json();
-
+  const pageDto = rsData.data;
   return (
     <div>
       <h1>글 목록</h1>
@@ -14,12 +14,46 @@ export default async function PostList() {
       <div>응답 코드 : {rsData.code}</div>
       <div>결과 메시지 : {rsData.msg}</div>
 
+      <div>totalPages : {pageDto.totalPages}</div>
+      <div>totalItems : {pageDto.totalItems}</div>
+      <div>currentPageNo : {pageDto.currentPageNo}</div>
+      <div>pageSize : {pageDto.pageSize}</div>
       <hr />
 
       <ul>
         <li>글1</li>
         <li>글2</li>
+        {pageDto.items.map((item: PostDto) => {
+          return (
+            <li className="border-2 border-red-500 my-2 p-2" key={item.id}>
+              <div>id : {item.id}</div>
+              <div>title : {item.title}</div>
+              <div>authorId : {item.authorId}</div>
+              <div>authorName : {item.authorName}</div>
+              <div>published : {item.published}</div>
+              <div>listed : {item.listed}</div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
 }
+type PostDto = {
+  id: number;
+  createDate: string;
+  modifyDate: string;
+  authorId: number;
+  authorName: string;
+  title: string;
+  published: boolean;
+  listed: boolean;
+};
+
+type PostItemPageDto = {
+  currentPageNumber: number;
+  pageSize: number;
+  totalPages: number;
+  totalItems: number;
+  items: PostDto[];
+};
