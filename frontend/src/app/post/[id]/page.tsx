233923acfdd8c9fc5613b20a86fa7e9a1c80827d
omegafrog <jiwoo22110103@gmail.com>
2025-02-16 11:25:25 +0900

    feat: add post info page

---
+++ frontend/src/app/post/[id]/page.tsx
@@ -0,0 +1,29 @@
+import createClient from "openapi-fetch";
+import ClientPage from "./ClientPage";
+import { paths } from "@/lib/backend/apiV1/schema";
+
+const client = createClient<paths>({
+  baseUrl: "http://localhost:8080",
+});
+
+export default async function Page({
+  params,
+}: {
+  params: {
+    id: number;
+  };
+}) {
+  const { id } = await params;
+
+  const response = await client.GET("/api/v1/posts/{id}", {
+    params: {
+      path: {
+        id,
+      },
+    },
+  });
+
+  const rsData = response.data!;
+  const post = rsData.data;
+  return <ClientPage post={post} />;
+}
