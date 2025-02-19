7a93e0133ae7f7cf405502e66c5de83a631a48eb
omegafrog <jiwoo22110103@gmail.com>
2025-02-19 10:43:13 +0900

    feat: add home list

--- frontend/src/app/ClientLayout.tsx
+++ frontend/src/app/ClientLayout.tsx
@@ -1,49 +1,92 @@
 "use client";
-import { Button, buttonVariants } from "@/components/ui/button";
 import client from "@/lib/backend/apiV1/fetchClient";
 import { components } from "@/lib/backend/apiV1/schema";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@radix-ui/react-dropdown-menu";
 import Link from "next/link";
 export default function ClinetLayout({
   children,
   me,
   fontVariable,
-  fontClassName
+  fontClassName,
 }: Readonly<{
   children: React.ReactNode;
-  me: components["schemas"]["MemberDto"],
-  fontVariable: string,
-  fontClassName: string
+  me: components["schemas"]["MemberDto"];
+  fontVariable: string;
+  fontClassName: string;
 }>) {
   const isLoggedIn = me.id !== 0;
+
+  async function handleLogout(e: React.MouseEvent<HTMLAnchorElement>) {
+    e.preventDefault();
+    const response = await client.DELETE("/api/v1/members/logout", {
+      credentials: "include",
+    });
+    if (response.error) {
+      alert(response.error.msg);
+      return;
+    }
+    // router.push(`/post/list`);
+    window.location.href = "/post/list";
+  }
   return (
     <html lang="en" className={`${fontVariable}`}>
       <body className={`min-h-[100dvh] flex flex-col ${fontClassName}`}>
-        <header className="flex gap-3">
-          <Link href="/">메인</Link>
-          <Link href="/about">소개</Link>
-          <Link href="/post/list">글 목록</Link>
-          {isLoggedIn && <Link className={buttonVariants({ variant: "outline" })} href="/post/write">글 작성</Link>}
-          {!isLoggedIn && <Link href="/member/login">로그인</Link>}
-          {!isLoggedIn && <Link href="/member/register">회원가입</Link>}
-          {isLoggedIn && <Link href="/member/me">내 정보</Link>}
-          {isLoggedIn && (
-            <Link
-              href=""
-              onClick={async (e) => {
-                e.preventDefault();
-                const response = await client.DELETE("/api/v1/members/logout", {
-                  credentials: "include",
-                });
-                if (response.error) {
-                  alert(response.error.msg);
-                  return;
-                }
-                window.location.href = "/post/list";
-              }}
-            >
-              로그아웃
-            </Link>
-          )}
+        <header className="flex justify-end gap-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger className="px-3 py-2">
+              <FontAwesomeIcon icon={faHouse} />
+              Home
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>{me.nickname}</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link href="/">메인</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/about">소개</Link>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <Link href="/post/list">글 목록</Link>
+              </DropdownMenuItem>
+              {isLoggedIn && (
+                <DropdownMenuItem>
+                  <Link href="/post/write">글 작성</Link>
+                </DropdownMenuItem>
+              )}
+              {!isLoggedIn && (
+                <DropdownMenuItem>
+                  <Link href="/member/login">로그인</Link>
+                </DropdownMenuItem>
+              )}
+              {!isLoggedIn && (
+                <DropdownMenuItem>
+                  <Link href="/member/join">회원 가입</Link>
+                </DropdownMenuItem>
+              )}
+              {isLoggedIn && (
+                <DropdownMenuItem>
+                  <Link href="" onClick={handleLogout}>
+                    로그아웃
+                  </Link>
+                </DropdownMenuItem>
+              )}
+              {isLoggedIn && (
+                <DropdownMenuItem>
+                  <Link href="/member/me">내정보</Link>
+                </DropdownMenuItem>
+              )}
+            </DropdownMenuContent>
+          </DropdownMenu>
         </header>
         <div className="flex-grow">{children}</div>
         <footer>푸터</footer>
